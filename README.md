6dded481549b9d7d7a760d1ff350918c9a7327d0
Mikhail Naletov <okgolove@markeloff.net>
2020-03-23 18:39:24 +0200

    First version of the module

---
+++ README.md
@@ -0,0 +1,57 @@
+# Jenkins (EC2) Terraform module
+
+## Usage
+
+```hcl
+module "jenkins" {
+  source = "git::https://github.com/DevopsCare/terraform-jenkins.git?ref=master"
+
+  source      = "../"
+  name        = "jenkins"
+  environment = "dev"
+  azs         = ["us-east-1a"]
+  cidr        = "10.0.0.0/16"
+
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
+
+  jenkins_ami_id = "ami-0bffbab661e4887b1"
+  keypair_name   = "dev"
+
+  zone_id                   = "Z12345643J95JTCWLP2AS"
+  jenkins_domain_name       = "jenkins.dev.example.com"
+  subject_alternative_names = ["jenkins-test.dev.example.com"]
+}
+```
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:-----:|
+| attributes | Additional attributes (e.g. `1`) | `list(string)` | `[]` | no |
+| azs | A list of availability zones in the region | `list(string)` | `[]` | no |
+| cidr | The CIDR block for the VPC which will be created if `vpc_id` is not specified | `string` | `""` | no |
+| delimiter | Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes` | `string` | `"-"` | no |
+| environment | Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT' | `string` | `""` | no |
+| ip\_whitelist | IP list will be able to access Jenkins | `list` | <code>["0.0.0.0/0"<br>]<br></code> | no |
+| jekins\_volume\_size | EC2 root volume size | `string` | `20` | no |
+| jenkins\_ami\_id | Jenkins AMI ID. It's recommended to use https://github.com/DevopsCare/packer-jenkins-ami for AMI building | `string` | `""` | no |
+| jenkins\_domain\_name | Domain will be used for ACM module | `string` | `""` | no |
+| jenkins\_instance\_type | The type of instance to start | `string` | `"t3.medium"` | no |
+| keypair\_name | Key pair will be used for EC2 instance | `string` | `""` | no |
+| name | Solution name, e.g. 'app' or 'jenkins' | `string` | `""` | no |
+| namespace | Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp' | `string` | `""` | no |
+| private\_subnet\_ids | A list of IDs of existing private subnets inside the VPC | `list(string)` | `[]` | no |
+| private\_subnets | A list of private subnets inside the VPC | `list(string)` | `[]` | no |
+| public\_subnet\_ids | A list of IDs of existing public subnets inside the VPC | `list(string)` | `[]` | no |
+| public\_subnets | A list of public subnets inside the VPC | `list(string)` | `[]` | no |
+| stage | Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release' | `string` | `""` | no |
+| subject\_alternative\_names | A list of domains that should be SANs in the issued certificate | `list(string)` | `[]` | no |
+| tags | Additional tags (e.g. `map('BusinessUnit','XYZ')` | `map(string)` | `{}` | no |
+| vpc\_id | ID of an existing VPC where resources will be created | `string` | `""` | no |
+| zone\_id | The ID of the hosted zone to contain Jenkins domain record | `string` | `""` | no |
+
+## Outputs
+
+No output.
+
---
+++ variables.tf
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2019 DevOps, SIA. - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the Apache License Version 2.0.
+ * http://www.apache.org/licenses
+ */
+
+##########
+# Naming #
+##########
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Solution name, e.g. 'app' or 'jenkins'"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+}
+
+#######
+# VPC #
+#######
+variable "vpc_id" {
+  description = "ID of an existing VPC where resources will be created"
+  type        = string
+  default     = ""
+}
+
+variable "public_subnet_ids" {
+  description = "A list of IDs of existing public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnet_ids" {
+  description = "A list of IDs of existing private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC which will be created if `vpc_id` is not specified"
+  type        = string
+  default     = ""
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+###########
+# Jenkins #
+###########
+variable "jenkins_instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "jenkins_ami_id" {
+  description = "Jenkins AMI id. It's recommended to use https://github.com/DevopsCare/packer-jenkins-ami for AMI building"
+  type        = string
+  default     = ""
+}
+
+variable "jekins_volume_size" {
+  description = "EC2 root volume size"
+  type        = string
+  default     = 20
+}
+
+variable "ip_whitelist" {
+  description = "IP list will be able to access Jenkins"
+  type        = list
+  default     = ["0.0.0.0/0"]
+}
+
+variable "keypair_name" {
+  description = "Key pair will be used for EC2 instance"
+  type        = string
+  default     = ""
+}
+
+#######
+# ACM #
+#######
+variable "jenkins_domain_name" {
+  description = "Domain will be used for ACM module"
+  type        = string
+  default     = ""
+}
+
+variable "zone_id" {
+  description = "The ID of the hosted zone to contain Jenkins domain record"
+  type        = string
+  default     = ""
+}
+
+variable "subject_alternative_names" {
+  description = "A list of domains that should be SANs in the issued certificate"
+  type        = list(string)
+  default     = []
+}
